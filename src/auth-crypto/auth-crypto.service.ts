2a3a7ec4a88e139c340475f9e0b79dc9e0ca642b
Guilherme GRO. dos Reis Olinto <guilhermeolinto@gac.local>
2022-11-11 16:54:23 -0300

    first commit

--- src/auth-crypto/auth-crypto.service.ts
+++ src/auth-crypto/auth-crypto.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import * as crypto from 'crypto'
+import * as crypto from 'crypto';
 @Injectable()
 export class AuthCryptoService {
+  async authService(body) {
+    const mensage = JSON.stringify(body);
+    const secret = 'guyi';
+    const hash = crypto
+      .createHmac('SHA512-224', secret)
+      .update(mensage)
+      .digest('hex');
+    console.log(hash);
 
-    async authService(body){
-        const mensage = JSON.stringify(body)
-        const secret = 'guyi';
-        const hash = crypto.createHmac('sha512', secret)
-                    .update(mensage)
-                    .digest('hex')
-        console.log(hash)
-    }
+    const hashArgon = await argon2.hash(hash);
+    console.log(hashArgon)
+  }
 }
